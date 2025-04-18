28827e6f29de244f273839cb2c2d819cb34f6043
Pablo <pf.paliza@gmail.com>
2025-04-18 18:05:57 -0400

    font

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import './App.css'
 
 function App() {
 
+    const sections = ["Work", 'About', 'Contact']
+
   return (
     <>
-      <div className='text-black'>
-        test
+      <div className='text-black font-haas'>
+          <div className='flex flex-row gap-x-4 text-4xl'>
+          {sections.map(section=>{
+              return <div style={{fontFamily: 'Neue Haas Grotesk'}}>{section}</div>
+          })}
+          </div>
       </div>
     </>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                haas: ["Neue Haas Grotesk", 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
