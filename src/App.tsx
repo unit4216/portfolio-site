30395f4263fccc4dda6a03ce1f0932de4b298f6c
Pablo <pf.paliza@gmail.com>
2025-04-20 14:32:36 -0400

    br

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export function MasonryGrid() {
                         }}
                     >
                         <div className='flex flex-col justify-end h-full'>
-                            <div className='bg-[#282828] text-[#F5F5F5] px-2 w-fit text-2xl'>
+                            <div className='bg-[#282828] text-[#F5F5F5] px-2 w-fit text-2xl rounded-lg'>
                                 {item.text}
                             </div>
                         </div>
