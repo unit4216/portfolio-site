bf87689d8011b2bb023b7baf1e53fd8b734ee734
Pablo <pf.paliza@gmail.com>
2025-04-26 12:08:28 -0400

    visual effects

--- src/pages/platformer-page/PlatformerPage.tsx
+++ src/pages/platformer-page/PlatformerPage.tsx
@@ -118,7 +118,7 @@ export const PlatformerPage = () => {
           top: position.y,
           imageRendering: "pixelated",
           transform: `translate(-50%, -50%) scaleX(${facing === "left" ? -1 : 1})`,
-          filter: "invert(1)",
+          filter: "saturate(2) contrast(5)",
         }}
       />
     </div>
