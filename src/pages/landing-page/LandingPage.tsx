7207b9ed059dda057248ee8ff0999630e4e406c8
Pablo <pf.paliza@gmail.com>
2025-07-04 08:38:55 -0400

    fix hover

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -281,7 +281,7 @@ export const LandingPage = () => {
                 onClick={() => scrollTo(section)}
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: index * 0.1 }}
+                transition={{ duration: 0.2 }}
                 whileHover={{ y: -2 }}
                 whileTap={{ scale: 0.95 }}
               >
