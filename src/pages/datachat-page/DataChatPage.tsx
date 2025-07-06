7f777f1849173f077fb4af1752b4fd39d963b7f7
Pablo <pf.paliza@gmail.com>
2025-07-06 11:26:52 -0400

    rm import

--- src/pages/datachat-page/DataChatPage.tsx
+++ src/pages/datachat-page/DataChatPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import initSqlJs, { Database } from "sql.js";
 import { GoogleGenAI } from "@google/genai";
 import { initializeDatabase } from "./databaseData";
