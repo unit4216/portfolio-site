3bbaff631285d5a7674dc2c862522dc244076ebb
Pablo <pf.paliza@gmail.com>
2025-07-31 10:03:32 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { LandingPage } from "./pages/landing-page/LandingPage.tsx";
 import { GauntletPage } from "./pages/gauntlet-page/GauntletPage.tsx";
 import { SamplerPage } from "./pages/sampler-page/SamplerPage.tsx";
 import DataChatPage from "./pages/datachat-page/DataChatPage.tsx";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/gauntlet" element={<GauntletPage />} />
-        <Route path="/sampler" element={<SamplerPage />} />
-        <Route path="/data-chat" element={<DataChatPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/gauntlet" element={<GauntletPage />} />
+          <Route path="/sampler" element={<SamplerPage />} />
+          <Route path="/data-chat" element={<DataChatPage />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
