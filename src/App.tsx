4b4a1ded25f0f9ce0bcaafb2f91b90dbcbf450aa
Pablo <pf.paliza@gmail.com>
2025-07-31 12:55:50 -0400

    rm gauntlet refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/landing-page/LandingPage.tsx";
-import { GauntletPage } from "./pages/gauntlet-page/GauntletPage.tsx";
 import { SamplerPage } from "./pages/sampler-page/SamplerPage.tsx";
 import DataChatPage from "./pages/datachat-page/DataChatPage.tsx";
 import { Analytics } from "@vercel/analytics/react"
@@ -14,7 +13,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/gauntlet" element={<GauntletPage />} />
           <Route path="/sampler" element={<SamplerPage />} />
           <Route path="/data-chat" element={<DataChatPage />} />
         </Routes>
