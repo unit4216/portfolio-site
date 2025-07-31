5afb094d2f55741206db8152146788636b47ee82
Pablo <pf.paliza@gmail.com>
2025-07-31 10:14:42 -0400

    reorder desc

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -148,7 +148,7 @@ const ResumeAccordion = () => {
         "Software Developer (2023-2024)", 
         "Junior Software Developer (2022-2023)"
       ],
-      description: "Progressive experience from Junior Developer to Lead, leading a team of 2 developers while building internal business applications using React, TypeScript, and Firebase. Cut cloud costs by 50% ($20K/year) and built data pipelines with BigQuery, Cloud Functions, and ETL scripts. Launched three new applications including an AI chatbot and an investment management system, while maintaining two existing core business systems."
+      description: "Progressive experience from Junior Developer to Lead, leading a team of 2 developers while building internal business applications using React, TypeScript, and Firebase.  Launched three new applications including an AI chatbot and an investment management system, while maintaining two existing core business systems. Cut cloud costs by 50% ($20K/year) and built data pipelines with BigQuery, Cloud Functions, and ETL scripts."
     },
     { 
       title: "Paliza Consulting", 
